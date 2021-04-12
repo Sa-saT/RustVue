f8d845855b6f97f00e5e12e0a2ee32148b149dbe
Sa-saT <sasa.is.music@gmail.com>
2021-04-12 16:30:02 +0900

    2020/04/12

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -36,6 +36,7 @@ fn handle_connection(mut stream: TcpStream) {
     let get = b"GET / HTTP/1.1\r\n";
     let get2 = b"GET /main.html HTTP/1.1\r\n";
     let sleep = b"GET /sleep HTTP/1.1\r\n";
+    let sleep2 = b"GET /main.html sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
         ("HTTP/1.1 200 OK\r\n\r\n", "index.html")
@@ -44,6 +45,9 @@ fn handle_connection(mut stream: TcpStream) {
         ("HTTP/1.1 200 OK\r\n\r\n", "index.html")
     } else if buffer.starts_with(get2) {
         ("HTTP/1.1 200 OK\r\n\r\n", "main.html")
+    } else if buffer.starts_with(sleep2) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK\r\n\r\n", "main.html") 
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
     };
